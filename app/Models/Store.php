ddfb003c7fe382bf7ab4b0f133103757bf2d52a9
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2024-02-18 03:12:51 +0800

    added category model, factory and migration

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Store extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'store_id';
+
+    public function details(): MorphMany {
+        return $this->morphMany(Detail::class, 'detailable');
+    }
 }
