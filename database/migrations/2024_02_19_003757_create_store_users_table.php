011961d34a255a91b8f0eed1736ad595f75290fd
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2024-02-19 09:23:05 +0800

    added storeusers model, factory and migration

---
+++ database/migrations/2024_02_19_003757_create_store_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_users', function (Blueprint $table) {
+            $table->id('store_user_id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+
+            $table->foreignId('store_id')
+                ->constrained('stores', 'store_id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_users');
+    }
+};
